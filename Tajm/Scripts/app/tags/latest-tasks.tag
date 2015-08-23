30c0c69a9c515f534d63a22f456facd9ba25246a
P-H Westman <per-hakan.westman@agrenshuset.se>
2015-08-23 17:53:38 +0200

    Fixed finished tasks table.

---
+++ Tajm/Scripts/app/tags/latest-tasks.tag
@@ -0,0 +1,39 @@
+﻿<latest-tasks>
+    <table>
+        <tr>
+            <th>Kund</th>
+            <th>Person</th>
+            <th>Uppgift</th>
+            <th>Start</th>
+            <th>Slut</th>
+        </tr>
+        <tr each="{latestTasks}">
+            <td>{customerId}</td>
+            <td>{employeeId}</td>
+            <td>{taskId}</td>
+            <td>{start}</td>
+            <td>{end.substring(0,10)}</td>
+        </tr>
+    </table>
+    <script>
+        var self = this;
+        self.latestTasks = [];
+        store.TaskTimes.on('collection_changed', function () {
+            self.latestTasks = store.TaskTimes.storeArray.filter(function (task) {
+                return task.end;
+            });
+            self.latestTasks.sort(function (a, b) {
+                if (a.end < b.end) {
+                    return 1;
+                }
+                if (a.end > b.end) {
+                    return -1;
+                }
+                // a must be equal to b
+                return 0;
+            });
+            self.update();
+        });
+
+    </script>
+</latest-tasks>
--- Tajm/Scripts/app/tags/tags.js
+++ Tajm/Scripts/app/tags/tags.js
@@ -95,6 +95,27 @@ riot.tag('add-tasktime', '<employee-dropdown name="EmployeeId" __selected="{task
         });
 
     
+});riot.tag('latest-tasks', '<table><tr><th>Kund</th><th>Person</th><th>Uppgift</th><th>Start</th><th>Slut</th></tr><tr each="{latestTasks}"><td>{customerId}</td><td>{employeeId}</td><td>{taskId}</td><td>{start}</td><td>{end.substring(0,10)}</td></tr></table>', function(opts) {
+        var self = this;
+        self.latestTasks = [];
+        store.TaskTimes.on('collection_changed', function () {
+            self.latestTasks = store.TaskTimes.storeArray.filter(function (task) {
+                return task.end;
+            });
+            self.latestTasks.sort(function (a, b) {
+                if (a.end < b.end) {
+                    return 1;
+                }
+                if (a.end > b.end) {
+                    return -1;
+                }
+
+                return 0;
+            });
+            self.update();
+        });
+
+    
 });riot.tag('worktask-dropdown', '<select name="{name}" onchange="{changeSelected}"><option each="{worktaskList}" id="{id}" __selected="{id==selected}">{name} ({price}:-)</option></select>', function(opts) {
         var self = this;
         this.name = opts.name || 'WorkTaskId';
