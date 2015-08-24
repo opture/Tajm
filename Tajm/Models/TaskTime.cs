aee9b99f90496f3d9f5dabd175846b6498b0b5b3
P-H Westman <per-hakan.westman@agrenshuset.se>
2015-08-24 12:23:13 +0200

    Added tasktimes.cs file.

---
+++ Tajm/Models/TaskTime.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Tajm.Models
+{
+    public class TaskTime
+    {
+        [Key]
+        public int Id { get; set; }
+        public virtual int CustomerId { get; set; }
+        [JsonIgnore]
+        public virtual Customer Customer { get; set; }
+        public virtual int EmployeeId { get; set; }
+        [JsonIgnore]
+        public virtual Employee Employee { get; set; }
+        public virtual int TaskId { get; set; }
+        [JsonIgnore]
+        public virtual WorkTask Task { get; set; }
+        public string Description { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime? End { get; set; }
+        public bool Invoiced { get; set; }
+
+    }
+}
